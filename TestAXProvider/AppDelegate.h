0b1e228466697e50d13a9ebd10aaab526c0b34fa
Nick Moore <nick@pilotmoon.com>
2013-05-01 08:12:29 +0100

    Initial commit

---
+++ TestAXProvider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAXProvider
+//
+//  Created by Nicholas Moore on 01/05/2013.
+//  Copyright (c) 2013 Nicholas Moore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TestAXProvider/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TestAXProvider
+//
+//  Created by Nicholas Moore on 01/05/2013.
+//  Copyright (c) 2013 Nicholas Moore. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
